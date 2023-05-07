0fd7a406a03686a82cce4136e820a914cdb9e97f
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2023-05-07 18:28:40 +0900

    feat: add global news page

--- src/components/global/GlobalCompoStyle.js
+++ src/components/global/GlobalCompoStyle.js
@@ -1,7 +1,122 @@
 import styled from "styled-components";
 
-export const GlobalCompoDiv = styled.div``;
+export const GlobalCompoDiv = styled.div`
+  display: flex;
+  flex-direction: column;
 
-export const GlobalTitleDiv = styled.div``;
+  box-sizing: border-box;
+  padding: 20px 0;
 
-export const GlobalNewsContainer = styled.div``;
+  width: 100vw;
+  height: 100vh;
+`;
+
+export const GlobalTitleDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+
+  gap: 10px;
+
+  box-sizing: border-box;
+  border-radius: 5px;
+
+  background-color: ${(props) => props.theme.lightColorBlur};
+  color: ${(props) => props.theme.bg};
+
+  width: 97%;
+  height: 180px;
+  padding: 20px;
+
+  svg {
+    width: 65px;
+    height: 65px;
+  }
+
+  #globalTitle {
+    font-size: 35px;
+    font-weight: 700;
+  }
+`;
+
+export const GlobalNewsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  box-sizing: border-box;
+
+  width: 98%;
+  height: 75%;
+  padding: 20px;
+
+  #globalText {
+    margin-bottom: 20px;
+    font-size: 15px;
+  }
+
+  #globalUl {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    justify-content: center;
+    align-items: center;
+    justify-items: center;
+    gap: 50px;
+
+    width: 100%;
+    height: auto;
+
+    overflow: scroll;
+    overflow-x: hidden;
+
+    .globalLi {
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+
+      width: 100%;
+      height: 100%;
+      padding: 20px;
+
+      background-color: rgb(255, 255, 255, 0.5);
+      color: ${(props) => props.theme.bg};
+
+      border-radius: 5px;
+
+      #articleImg {
+        width: 100%;
+        height: 50%;
+
+        border-radius: 5px;
+      }
+
+      #articleTitle {
+        margin-top: 10px;
+        font-size: 20px;
+        font-weight: 500;
+      }
+
+      .authorDiv {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        margin-top: 7px;
+        margin-bottom: 15px;
+
+        #articleAuthor {
+          font-size: 15px;
+          font-weight: 500;
+        }
+
+        #articleDate {
+          font-size: 12px;
+          font-weight: 400;
+        }
+      }
+
+      #articleContents {
+        font-size: 15px;
+        line-height: 19px;
+      }
+    }
+  }
+`;
--- src/components/global/global.jsx
+++ src/components/global/global.jsx
@@ -1,21 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import {
   GlobalCompoDiv,
   GlobalTitleDiv,
   GlobalNewsContainer,
 } from "./GlobalCompoStyle";
 import { BsGlobe2 } from "react-icons/bs";
+import dummyImg from "../../images/dummy-image.jpg";
 
 const global = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        // react에서 env 파일 설정시 REACT_APP_을 붙여주어야 한다.
+        const API_KEY = process.env.REACT_APP_API_KEY;
+        const GLOBAL_API = `https://newsapi.org/v2/everything?q=global&apiKey=${API_KEY}`;
+        const datas = await axios.get(GLOBAL_API);
+        const articles = datas.data.articles;
+
+        const filterItem = articles.filter((item) => {
+          return item.source.name !== "Google News";
+        });
+        setData(filterItem);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    getData();
+  }, []);
+
   return (
     <GlobalCompoDiv>
       <GlobalTitleDiv>
         <BsGlobe2 id="globalSvg" />
-        <p id="globalTitle">global</p>
+        <p id="globalTitle">Global</p>
       </GlobalTitleDiv>
       <GlobalNewsContainer>
-        <p id="globalText">global news pick one !</p>
-        <ul id="globalUl"></ul>
+        <ul id="globalUl">
+          {data.map((item) => {
+            const title = item.title;
+            const content = `${item.content}`;
+            const author = item.source.name;
+            const image = item.urlToImage;
+            const url = item.url;
+            const publishDate = item.publishedAt;
+
+            return (
+              <li className="globalLi" key={publishDate}>
+                <img
+                  id="articleImg"
+                  src={image ? image : dummyImg}
+                  alt="articleImg"
+                />
+                <p id="articleTitle">
+                  <a href={url}>{title}</a>
+                </p>
+                <div className="authorDiv">
+                  <p id="articleAuthor">{author ? author : "anonymous"}</p>
+                  <p id="articleDate">{publishDate.slice(0, 10)}</p>
+                </div>
+
+                <p id="articleContents">{content}</p>
+              </li>
+            );
+          })}
+        </ul>
       </GlobalNewsContainer>
     </GlobalCompoDiv>
   );
